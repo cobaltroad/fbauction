2bc068e4bc6a248f89cebf90feaf9c6a69d4e761
Ron Dollete <rdollete@ford.com>
2019-03-27 11:13:55 -0400

    Provide rating based on individual stat

--- src/main/java/com/cobaltroad/fbauction/service/LeagueStatService.java
+++ src/main/java/com/cobaltroad/fbauction/service/LeagueStatService.java
@@ -34,21 +34,87 @@ public class LeagueStatService {
 
     public double battingAverageRatingFor(HitterProjection projection) {
         String league = projection.getLeague();
-        double muHits = hitterProjectionRepository.averageOfAllHits(league);
-        double sigmaHits = hitterProjectionRepository.stddevOfAllHits(league);
+        double muHits = getLeagueStat(league + ":average:hits");
+        double sigmaHits = getLeagueStat(league + ":stddev:hits");
 
         int projectionHits = projection.getHits();
         double devsFromMeanHits = (projectionHits - muHits) / sigmaHits;
-        System.out.println("Deviations From Mean (Hits): " + devsFromMeanHits);
 
-        double muAtBats = hitterProjectionRepository.averageOfAllABs(league);
-        double sigmaAtBats = hitterProjectionRepository.stddevOfAllABs(league);
+        double muAtBats = getLeagueStat(league + ":average:atbats");
+        double sigmaAtBats = getLeagueStat(league + ":stddev:atbats");
 
         int projectionAtBats = projection.getAtBats();
         double devsFromMeanAtBats = (projectionAtBats - muAtBats) / sigmaAtBats;
-        System.out.println("Deviations From Mean (ABs): " + devsFromMeanAtBats);
 
         return devsFromMeanHits - devsFromMeanAtBats;
+    }
+
+    public void aggregateRuns(String league) {
+        double mu = hitterProjectionRepository.averageOfAllRuns(league);
+        double sigma = hitterProjectionRepository.stddevOfAllRuns(league);
+
+        saveLeagueStat(league + ":average:runs", mu);
+        saveLeagueStat(league + ":stddev:runs", sigma);
+    }
+
+    public double runsRatingFor(HitterProjection projection) {
+        String league = projection.getLeague();
+        double mu = getLeagueStat(league + ":average:runs");
+        double sigma = getLeagueStat(league + ":stddev:runs");
+
+        int projectedStat = projection.getRuns();
+        return (projectedStat - mu) / sigma;
+    }
+
+    public void aggregateRBIs(String league) {
+        double mu = hitterProjectionRepository.averageOfAllRBIs(league);
+        double sigma = hitterProjectionRepository.stddevOfAllRBIs(league);
+
+        saveLeagueStat(league + ":average:rbi", mu);
+        saveLeagueStat(league + ":stddev:rbi", sigma);
+    }
+
+    public double rbiRatingFor(HitterProjection projection) {
+        String league = projection.getLeague();
+        double mu = getLeagueStat(league + ":average:rbi");
+        double sigma = getLeagueStat(league + ":stddev:rbi");
+
+        int projectedStat = projection.getRunsBattedIn();
+        return (projectedStat - mu) / sigma;
+    }
+
+    public void aggregateHRs(String league) {
+        double mu = hitterProjectionRepository.averageOfAllHRs(league);
+        double sigma = hitterProjectionRepository.stddevOfAllHRs(league);
+
+        saveLeagueStat(league + ":average:hr", mu);
+        saveLeagueStat(league + ":stddev:hr", sigma);
+    }
+
+    public double homerunRatingFor(HitterProjection projection) {
+        String league = projection.getLeague();
+        double mu = getLeagueStat(league + ":average:hr");
+        double sigma = getLeagueStat(league + ":stddev:hr");
+
+        int projectedStat = projection.getHomeruns();
+        return (projectedStat - mu) / sigma;
+    }
+
+    public void aggregateSBs(String league) {
+        double mu = hitterProjectionRepository.averageOfAllSBs(league);
+        double sigma = hitterProjectionRepository.stddevOfAllSBs(league);
+
+        saveLeagueStat(league + ":average:sb", mu);
+        saveLeagueStat(league + ":stddev:sb", sigma);
+    }
+
+    public double stolenBaseRatingFor(HitterProjection projection) {
+        String league = projection.getLeague();
+        double mu = getLeagueStat(league + ":average:sb");
+        double sigma = getLeagueStat(league + ":stddev:sb");
+
+        int projectedStat = projection.getStolenBases();
+        return (projectedStat - mu) / sigma;
     }
 
     private void saveLeagueStat(String key, double value) {
@@ -59,6 +125,11 @@ public class LeagueStatService {
             ls.setValue(value);
             leagueStatRepository.save(ls);
         }
+    }
+
+    private double getLeagueStat(String key) {
+        LeagueStat ls = leagueStatRepository.findFirstByKey(key);
+        return ls.getValue();
     }
 
 }
--- src/test/java/com/cobaltroad/fbauction/service/LeagueStatServiceTest.java
+++ src/test/java/com/cobaltroad/fbauction/service/LeagueStatServiceTest.java
@@ -51,4 +51,89 @@ public class LeagueStatServiceTest {
         assertEquals( 0.31, service.battingAverageRatingFor(projection2), 0.01);
         assertEquals(-0.15, service.battingAverageRatingFor(projection3), 0.01);
     }
+
+    @Test
+    public void aggregateRuns() {
+        HitterProjection projection1 = HitterProjection.builder().league("nl").runs(50).build();
+        hitterProjectionRepository.save(projection1);
+
+        HitterProjection projection2 = HitterProjection.builder().league("nl").runs(100).build();
+        hitterProjectionRepository.save(projection2);
+
+        HitterProjection projection3 = HitterProjection.builder().league("nl").runs(80).build();
+        hitterProjectionRepository.save(projection3);
+
+        service.aggregateRuns("nl");
+
+        assertEquals(76.67, leagueStatRepository.valueOf("nl:average:runs"), 0.01);
+        assertEquals(25.17, leagueStatRepository.valueOf("nl:stddev:runs"), 0.01);
+
+        assertEquals(-1.06, service.runsRatingFor(projection1), 0.01);
+        assertEquals( 0.93, service.runsRatingFor(projection2), 0.01);
+        assertEquals( 0.13, service.runsRatingFor(projection3), 0.01);
+    }
+
+    @Test
+    public void aggregateRBIs() {
+        HitterProjection projection1 = HitterProjection.builder().league("nl").runsBattedIn(50).build();
+        hitterProjectionRepository.save(projection1);
+
+        HitterProjection projection2 = HitterProjection.builder().league("nl").runsBattedIn(100).build();
+        hitterProjectionRepository.save(projection2);
+
+        HitterProjection projection3 = HitterProjection.builder().league("nl").runsBattedIn(80).build();
+        hitterProjectionRepository.save(projection3);
+
+        service.aggregateRBIs("nl");
+
+        assertEquals(76.67, leagueStatRepository.valueOf("nl:average:rbi"), 0.01);
+        assertEquals(25.17, leagueStatRepository.valueOf("nl:stddev:rbi"), 0.01);
+
+        assertEquals(-1.06, service.rbiRatingFor(projection1), 0.01);
+        assertEquals( 0.93, service.rbiRatingFor(projection2), 0.01);
+        assertEquals( 0.13, service.rbiRatingFor(projection3), 0.01);
+    }
+
+    @Test
+    public void aggregateHRs() {
+        HitterProjection projection1 = HitterProjection.builder().league("nl").homeruns(50).build();
+        hitterProjectionRepository.save(projection1);
+
+        HitterProjection projection2 = HitterProjection.builder().league("nl").homeruns(100).build();
+        hitterProjectionRepository.save(projection2);
+
+        HitterProjection projection3 = HitterProjection.builder().league("nl").homeruns(80).build();
+        hitterProjectionRepository.save(projection3);
+
+        service.aggregateHRs("nl");
+
+        assertEquals(76.67, leagueStatRepository.valueOf("nl:average:hr"), 0.01);
+        assertEquals(25.17, leagueStatRepository.valueOf("nl:stddev:hr"), 0.01);
+
+        assertEquals(-1.06, service.homerunRatingFor(projection1), 0.01);
+        assertEquals( 0.93, service.homerunRatingFor(projection2), 0.01);
+        assertEquals( 0.13, service.homerunRatingFor(projection3), 0.01);
+    }
+
+    @Test
+    public void aggregateSBs() {
+        HitterProjection projection1 = HitterProjection.builder().league("nl").stolenBases(50).build();
+        hitterProjectionRepository.save(projection1);
+
+        HitterProjection projection2 = HitterProjection.builder().league("nl").stolenBases(100).build();
+        hitterProjectionRepository.save(projection2);
+
+        HitterProjection projection3 = HitterProjection.builder().league("nl").stolenBases(80).build();
+        hitterProjectionRepository.save(projection3);
+
+        service.aggregateSBs("nl");
+
+        assertEquals(76.67, leagueStatRepository.valueOf("nl:average:sb"), 0.01);
+        assertEquals(25.17, leagueStatRepository.valueOf("nl:stddev:sb"), 0.01);
+
+        assertEquals(-1.06, service.stolenBaseRatingFor(projection1), 0.01);
+        assertEquals( 0.93, service.stolenBaseRatingFor(projection2), 0.01);
+        assertEquals( 0.13, service.stolenBaseRatingFor(projection3), 0.01);
+    }
+
 }
